ff75b139c005d8244be8310b696aa76a705ada09
Steven Greenberg <sgreenberg@pfpcyber.com>
2015-10-19 11:59:00 -0400

    Added poll

--- gotobot.py
+++ gotobot.py
@@ -15,6 +15,7 @@ interns = ["Jon", "Yura", "Alex", "Avik", "Derek", "Tommy"]
 people = interns + ["Omar", "David", "Alan", "Alison", "Bulent", "Carlos", "Jeff", "Steven", "Thurston", "Linda"]
 timestamp = queue.Queue()
 last_channel = ""
+polls = []
 def startBot():
     print(datetime.datetime.now())
     # g = git.cmd.Git("C:\\Users\\D\\pfpui")
@@ -35,6 +36,7 @@ def startBot():
                     print ("[!!] error in message, restarting bot")
                     error = "error - no quotes found"
                     print(sc)
+                    last_channel = msg["channel"]
                     sc.rtm_send_message(last_channel, error)
                     startBot()
                 #print("type" in msg and msg["type"] == "message"and "text" in msg)
@@ -63,10 +65,8 @@ def startBot():
                             print("poll")
                             startPoll(msg)
                         elif("~stoppoll" in msg["text"].lower()):
-                            pass
                             stopPoll(msg)
                         elif("~vote" in msg["text"].lower()):
-                            pass
                             vote(msg)
                         elif("~deleteall" in msg["text"].lower()):
                             while not timestamp.empty():
@@ -79,7 +79,6 @@ def startBot():
                                 ts = timestamp.get()
                                 for w in whitelist:
                                     sc.api_call("chat.delete",channel=w, ts=str(ts["ts"]))
-                        #sc.rtm_send_message(msg["channel"], msg["text"])
                 elif("ok" in msg and msg["ok"] == True):
                     timestamp.put({"ts":msg["ts"],"channel":last_channel})
             elif(len(msg) > 1):
@@ -129,8 +128,7 @@ def quote(msg):
             else:
                 with open(fileName, "a+") as f:
                     f.write(args[2])
-            
-                
+            last_channel = msg["channel"]
             sc.rtm_send_message(msg["channel"], "Quote added " + args[2])
     elif(len(args) == 2):
         print(2)
@@ -144,8 +142,83 @@ def quote(msg):
                 last_channel = msg["channel"]
                 sc.rtm_send_message(msg["channel"], random.choice(quotes))
 
+def findName(ds, nam):
+    for d in ds:
+        if("name" in d):
+            if(d["name"] == nam):
+                return d
+    return None
+
+
 def startPoll(msg):
-    pass
+    global polls
+    global last_channel
+    args = msg["text"].split(",")
+    last_channel = msg["channel"]
+    if(len(args) > 4):
+        poll = {"name":args[1]}
+        i = 2
+        while(i < len(args)):
+            poll[args[i]] = 0
+            i += 1
+        polls.append(poll)
+        sc.rtm_send_message(msg["channel"], "Poll created")
+    else:
+        sc.rtm_send_message(msg["channel"], "Not enough arguments")
+
+def vote(msg):
+    global polls
+    global last_channel
+    args = msg["text"].split(",")
+    last_channel = msg["channel"]
+    if(len(args) == 3):
+        d = findName(polls, args[1])
+        if(d != None):
+            if(args[2] in d):
+                d[args[2]] += 1
+                printPoll(d,msg)
+            else:
+                sc.rtm_send_message(msg["channel"], "Invalid vote option")
+        else:
+            sc.rtm_send_message(msg["channel"], "Could not find poll")
+    else:
+        sc.rtm_send_message(msg["channel"], "Incorrect number of arugments")
+
+def stopPoll(msg):
+    print("endpoll")
+    global polls
+    global last_channel
+    args = msg["text"].split(",")
+    last_channel = msg["channel"]
+    if(len(args) == 2):
+        d = findName(polls, args[1])
+        if(d != None):
+            polls.remove(d)
+            printPoll(d, msg)
+        else:
+            sc.rtm_send_message(msg["channel"], "Could not find poll")
+    else:
+        sc.rtm_send_message(msg["channel"], "Incorrect number of arugments")
+
+def printPoll(poll, msg):
+    global polls
+    global last_channel
+    #p = findName(polls,name).copy()
+    p = poll.copy()
+    name = p["name"]
+    del(p["name"])
+    s = "```" + name
+    for w in sorted(p, key=p.get, reverse=True):
+        s += "\n  " + str(w) + (" " * (10 - len(w))) + " " + str(p[w])
+    s += "```"
+    #print(s)
+    last_channel = msg["channel"]
+    sc.rtm_send_message(msg["channel"], s)
+
+
+    
+
+
 
 #def send(msg):
 

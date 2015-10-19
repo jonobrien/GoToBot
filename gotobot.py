73d13853ce24d6608ecc03663ea2b7e950da21df
Jon O'Brien <jvo7822@rit.edu>
2015-10-19 16:00:15 -0400

    syntax changes

--- gotobot.py
+++ gotobot.py
@@ -22,7 +22,7 @@ with open("token.txt", "r") as tRead:
          token = tRead.read()
 #global sc
 sc = SlackClient(token)
-interns = ["Jon", "Yura", "Alex", "Avik", "StevieG", "Tommy"]
+interns = ["Jon", "Yura", "Alex", "Avik", "StevieG", "Tommy"]*3+["Alex"]
 people = interns + ["Omar", "David", "Alan", "Alison", "Bulent", "Carlos", "Jeff", "Steven", "Thurston", "Linda","Derek"]
 timestamp = queue.Queue()
 last_channel = ""
